45dd198928b9d254011647e9fcde383634e94a76
Mj <michaelpics52@gmail.com>
2024-10-20 22:07:18 -0500

    we heard you like logic so we put a gate in ur gate so you can logic while you logic

--- basic_ops.cpp
+++ basic_ops.cpp
@@ -1,15 +1,19 @@
-int addition(int a, int b) {
-    while (b != 0) {
-        int carry = a & b;  // Calculate carry bits
-        a = a ^ b;          // Sum bits without carry
-        b = carry << 1;     // Shift carry to left by one place
+int addition(int a, int b)
+{
+    while (b != 0)
+    {
+        int carry = a & b; // Calculate carry bits
+        a = a ^ b;         // Sum bits without carry
+        b = carry << 1;    // Shift carry to left by one place
     }
     return a;
 }
-int subtraction(int a, int b) {
-    while (b != 0) {
+int subtraction(int a, int b)
+{
+    while (b != 0)
+    {
         // Borrow contains common set bits of a and b
-        int borrow = (~a) & b;  // Where we need to borrow
+        int borrow = (~a) & b; // Where we need to borrow
         // Subtraction without borrow
         a = a ^ b;
         // Borrow is shifted by 1 so that it can be subtracted from the next higher bit
@@ -17,10 +21,12 @@ int subtraction(int a, int b) {
     }
     return a;
 }
-int twos_subtract(int a, int b) {
+int twos_subtract(int a, int b)
+{
     // Subtract by adding two's complement of b
     return addition(a, addition(~b, 1));
 }
-int twos_addition(int a, int b) {
-    return subtract(a, subtract(~b, 1));
+int twos_addition(int a, int b)
+{
+    return subtraction(a, subtraction(~b, 1));
 }
