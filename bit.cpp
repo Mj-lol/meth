07d6deb66bce24fee94ddd8c580958fcfa286aa0
Mj <michaelpics52@gmail.com>
2024-10-24 03:02:41 -0500

    i have joined a union

--- bit.cpp
+++ bit.cpp
@@ -1,5 +1,5 @@
 #include "logic gate.cpp"
-
+#include "iostream"
 struct bit // equivalent to memory cell (should we consider )
 {
     unsigned int value : 1;
@@ -93,14 +93,82 @@ struct bit // equivalent to memory cell (should we consider )
     }
 };
 
-struct byte
+union byte
 {
-    bit b0;
-    bit b1;
-    bit b2;
-    bit b3;
-    bit b4;
-    bit b5;
-    bit b6;
-    bit b7;
+    struct
+    {
+        unsigned int b0 : 1;
+        unsigned int b1 : 1;
+        unsigned int b2 : 1;
+        unsigned int b3 : 1;
+        unsigned int b4 : 1;
+        unsigned int b5 : 1;
+        unsigned int b6 : 1;
+        unsigned int b7 : 1;
+    };
+    unsigned char value;
+
+    void setBit(int index, bool state)
+    {
+        if (index < 0 || index > 7)
+            throw std::out_of_range("Bit index out of range");
+        switch (index)
+        {
+        case 0:
+            b0 = state;
+            break;
+        case 1:
+            b1 = state;
+            break;
+        case 2:
+            b2 = state;
+            break;
+        case 3:
+            b3 = state;
+            break;
+        case 4:
+            b4 = state;
+            break;
+        case 5:
+            b5 = state;
+            break;
+        case 6:
+            b6 = state;
+            break;
+        case 7:
+            b7 = state;
+            break;
+        }
+    }
+
+    bool getBit(int index) const
+    {
+        if (index < 0 || index > 7)
+            throw std::out_of_range("Bit index out of range");
+        switch (index)
+        {
+        case 0:
+            return b0;
+        case 1:
+            return b1;
+        case 2:
+            return b2;
+        case 3:
+            return b3;
+        case 4:
+            return b4;
+        case 5:
+            return b5;
+        case 6:
+            return b6;
+        case 7:
+            return b7;
+        }
+        return false;
+    }
+
+    void toggleBit(int index)
+    {
+        setBit(index, !getBit(index));
+    }
 };
